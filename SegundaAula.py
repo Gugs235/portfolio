dc304ef88a34a994752175744afda363b078496c
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-09-23 18:19:14 -0300

    primeiro commit de texte EX:Fibonati

--- SegundaAula.py
+++ SegundaAula.py
@@ -51,8 +51,8 @@ print("\n")
 print("+" + 20 * "-" + "+")
 print(("|" + 4 * " " + "|" + 9 * " " + "|" + 5 * " " + "|\n") * 2, end = "")
 print(("|" + " " * 20 + "|\n") * 5, end = "")
-print("|" + 2 * " " + 15 * "#" +  3 * " " + "|")
-print("+" + 20 * "-" + "+")
+print("|" + 3 * " " + 15 * "#" +  3 * " " + "|")
+print("+" + 21 * "-" + "+")
 
 # Foramatação de strings 2
 frase = "Um triangulo de base igual a {0} e altura igual a {1} possui área igual a {2}.".format(2,4,12)

401a8f3df84247240eba1add22dd43558017fa12
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-10-01 18:32:05 -0300

    Proj. dia 01/10/24

---
+++ Ex 01 listas.py
@@ -0,0 +1,12 @@
+# receba 5 numeros
+# mostre-os
+# mostre o maior
+# mostre o menor
+# mostre quantos tem
+# some todos
+lista = [1, 2, 3, 4, 5]
+print(lista)
+print(max(lista))
+print(min(lista))
+print(len(lista))
+print(sum(lista))

afc87d4a7a792f374f71d3d7265dfbd617ba41a3
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-10-17 17:53:06 -0300

    Proj. dia 15/10/24 (CONCLUIDO)

--- Ex 12 - posto de gasolina.py
+++ Ex 12 - posto de gasolina.py
@@ -2,11 +2,27 @@ print("G - Gasolina")
 print("A - Álcool")
 resp = str(input("digite o que você deseja? "))
 
-if resp == "G":
+if resp == "G" or resp == "g":
     print("Sua escolha foi gasolina")
     litros = float(input("Quantos litros você deseja? "))
     
     preco = litros * 2.50
+
+    if litros <= 20:
+        desc = preco * 0.04
+        final = preco - desc
+    elif litros > 20:
+        desc = preco * 0.06
+        final = preco - desc
+    print(litros)
+    print(final)
+
+
+elif resp == "A" or resp == "a":
+    print("Sua escolha foi álcool")
+    litros = float(input("Quantos litros você deseja? "))
+
+    preco = litros * 1.90
 
     if litros <= 20:
         desc = preco * 0.03
@@ -16,8 +32,3 @@ if resp == "G":
         final = preco - desc
     print(litros)
     print(final)
-
-
-elif resp == "A":
-    print("Sua escolha foi álcool")
-    litros = float(input("Quantos litros você deseja? "))

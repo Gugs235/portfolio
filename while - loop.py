546e6ef7205b9213a64d9cba5b21c08e1906bfa8
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-10-17 15:24:30 -0300

    Proj. dia 17/10/24
    primeira aula de laço de repetição.

---
+++ while - loop.py
@@ -0,0 +1,13 @@
+# break - encerra um codigo
+
+# while True - loop infinito
+# Ctrl + C faz ele parar
+while True:
+    valor = int(input("Digite 1 ou 0 para encerrar: "))
+    if valor >= 1:
+        # continue volta para o laço
+        print("Maior que um")
+    if valor >= 0:
+        print("Maior que um")
+        break
+1

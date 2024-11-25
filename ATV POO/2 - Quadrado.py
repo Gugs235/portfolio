d8113b1f4efd5789f019be876488efd6b06f2342
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-11-25 16:20:26 -0300

    Atualização 2 - Quadrado.py
    25/11/24
    Eu atualizei o projeto, pois ainda não estava funcionando.

--- ATV POO/2 - Quadrado.py
+++ ATV POO/2 - Quadrado.py
@@ -4,20 +4,19 @@
     # mostrar_area
     # mudar_valor_lado (retorna o valor do lado e calcula area)
 
-class Quadrado():
+class Quadrado:
     def __init__(self, tamanho_do_lado):
         self.tamanho_do_lado = tamanho_do_lado
 
     def mostrar_area(self):
-        print(f"Sua area é: ")
+        area = self.tamanho_do_lado ** 2
+        print(f"Sua área é: {area}")
 
     def mudar_valor_lado(self):
-        lado = input("Qual é a nova cor? ")
-        return lado
-
+        novo_lado = float(input("Qual é o novo tamanho do lado? "))
+        self.tamanho_do_lado = novo_lado
 
 qdd1 = Quadrado(50)
-area = lado * lado
-print(f"A área é {qdd1.cor}")
-qdd1.troca_cor()
-qdd1.mostra_cor()
+qdd1.mostrar_area()
+qdd1.mudar_valor_lado()
+qdd1.mostrar_area()

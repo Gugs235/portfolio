48bbf43e37421f4e88e4ac0051a281609090dce4
Gugs235 <jhonathan.ssoares2008@gmail.com>
2025-02-06 16:17:45 -0400

    Proj. dia 06/02/25
    Quarta atividade, mas é a primeira vez que consegui subir o código para o github.

---
+++ MySQL/atv4_escola3.sql
@@ -0,0 +1,90 @@
+create database atv4_escola3;
+
+use atv4_escola3;
+
+create table aluno (
+	matricula int primary key auto_increment,
+    nome varchar(50) not null,
+    email varchar(50) not null
+);
+
+drop table Aluno;
+
+insert into aluno (matricula, nome, email) values (null, "Jhonathan", "jhon@soares"),
+(null, "Anne", "anne@primon"),
+(null, "Celso", "celso@primon"),
+(null, "Luiz", "luiz@aluno"),
+(null, "Igor", "Igor@aluno");
+
+create table estado (
+	id int primary key auto_increment,
+	nome_estado varchar(50) not null,
+    sigla varchar(50) not null
+);
+
+drop table estado;
+
+insert into estado (id, nome_estado, sigla) values (null, "Acre", "AC"),  
+(null, "Alagoas", "AL"),  
+(null, "Amapá", "AP"),  
+(null, "Amazonas", "AM"),  
+(null, "Bahia", "BA"),  
+(null, "Ceará", "CE"),  
+(null, "Distrito Federal", "DF"),  
+(null, "Espírito Santo", "ES"),  
+(null, "Goiás", "GO"),  
+(null, "Maranhão", "MA"),  
+(null, "Mato Grosso", "MT"),  
+(null, "Mato Grosso do Sul", "MS"),  
+(null, "Minas Gerais", "MG"),  
+(null, "Pará", "PA"),  
+(null, "Paraíba", "PB"),  
+(null, "Paraná", "PR"),  
+(null, "Pernambuco", "PE"),  
+(null, "Piauí", "PI"),  
+(null, "Rio de Janeiro", "RJ"),  
+(null, "Rio Grande do Norte", "RN"),  
+(null, "Rio Grande do Sul", "RS"),  
+(null, "Rondônia", "RO"),  
+(null, "Roraima", "RR"),  
+(null, "Santa Catarina", "SC"),  
+(null, "São Paulo", "SP"),  
+(null, "Sergipe", "SE"),  
+(null, "Tocantins", "TO"); 
+
+select * from aluno;
+
+create table cidade (
+	id int primary key auto_increment,
+	nome varchar(50) not null,
+    id_estado int not null,
+    foreign key (id_estado)references estado (id),
+    região varchar(50) not null
+);
+
+drop table cidade;
+
+insert into cidade (id, nome, id_estado, região) values  
+(null, "São Paulo", 12, "Sudeste"),  
+(null, "Rio de Janeiro", 7, "Sudeste"),  
+(null, "Belo Horizonte", 19, "Sudeste"),  
+(null, "Vitória", 3, "Sudeste"),  
+(null, "Curitiba", 25, "Sul"),  
+(null, "Porto Alegre", 8, "Sul"),  
+(null, "Florianópolis", 14, "Sul"),  
+(null, "Salvador", 22, "Nordeste"),  
+(null, "Recife", 6, "Nordeste"),  
+(null, "Fortaleza", 17, "Nordeste"),  
+(null, "São Luís", 9, "Nordeste"),  
+(null, "Natal", 1, "Nordeste"),  
+(null, "João Pessoa", 16, "Nordeste"),  
+(null, "Brasília", 27, "Centro-Oeste"),  
+(null, "Goiânia", 11, "Centro-Oeste"),  
+(null, "Cuiabá", 4, "Centro-Oeste"),  
+(null, "Campo Grande", 20, "Centro-Oeste"),  
+(null, "Manaus", 2, "Norte"),  
+(null, "Belém", 13, "Norte"),  
+(null, "Palmas", 23, "Norte");  
+
+select * from cidade;
+
